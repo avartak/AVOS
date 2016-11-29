4a1caa6e49ab3ab81331890d7f587d002a5ba578
Adish <adish.vartak@cern.ch>
2016-11-29 20:37:14 +0100

    Printing to screen using video buffer

--- bootloader.asm
+++ bootloader.asm
@@ -22,24 +22,75 @@ BITS 16
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 Kernel:
 
-	; The following code can be thought of as a code module that prints a string to screen
-	; We will use the BIOS interrupt service routine 0x10 to do the printing
+	; We should initialize the segment registers to the base-address values that we want to use
+	; We should not assume the segment registers to be initialized in a certain way when we are handed control
+	; The segment registers known in 16-bit environment are : CS, DS, ES, SS
+	; Physical address given by reg:add combination is : [reg] x 0x100 + add
+	; This allows us to address a memory range starting from 0 to 0xFFFF00+0xFFFF
+	; If you do the math you will find that this range exceeds the 1 MB address space that a 20-bit address bus can physically access
+	; By default the addresses beyond the 1 MB mark get looped back to 0
+	; However, in reality we now have processors capable of physically accessing addresses beyond 1 MB
+	; To enable access to this >1MB address space in real mode, we would need to enable the A20 line
+	; We will do that later
+	
+	; Now, we will just initialize the registers DS, ES to 0 
 
-	mov si, Welcome_Message	                  ; Load the string pointer into the SI register
-	mov ah, 0Eh		                  ; We want a teletype output - printing basic characters to the screen 
-	.printchar:                               ; We set up a loop here to print every single character
-		lodsb			          ; Load the byte from the address in SI into AL, then increment the address in SI
-		cmp al, 0                         ; Have we reached the end of the string ?
-		je .done		          ; If so, break out of the loop
-		int 10h                           ; Call the BIOS routine 0x10 to print the character stored in AL 
-		jmp .printchar                    ; Continue the printing loop
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+	
+	; We should also create a stack 
+	; Our OS is quite small, so let us set up the stack at 0x8000
+	; Let us be generous and allocate 4 KB to the stack
+	; This means putting 4096 in the stack pointer SP - remember the stack grows downwards 
 
-	.done:                                    ; Printing is done
+	mov ax, 0x8000
+	mov ss, ax
+	mov sp, 4096
 
-	; The OS has performed its goal in life
+	; We now call the 'Print' function to display our welcome message (function defined later)
+	; After printing the welcome message the OS has performed its goal in life
 
+	call Print
 	cli                                       ; Clear all interrupts so that we won't be disturbed            
 	hlt                                       ; Halt the system	
+
+; The Print function - displays a string on screen
+; We will write directly to the video buffer
+; The function first clears the scree, then displays the welcome message
+
+Print:
+        pusha                                     ; We start by pushing all the general-purpose registers onto the stack
+	                                          ; This way we can restore their values after the function returns 
+        push es                                   ; Push the ES register onto the stack - we will use it to access the video memory
+        mov ax, 0xB800                            ; In the PC architecture the video buffer sits at 0xB8000
+        mov es, ax                                ; We put the ES segment at that location
+        mov di, 0                                 ; We start writing from the beginning of the video buffer
+        mov ax, 80                                ; There are 80 columns
+        mov bx, 25                                ; And 25 rows
+        mul bx                                    ; Total number of characters we can print: 80x25
+        mov cx, ax                                ; Set this number in the counter register - to be used for clearing the creen
+
+	.clearscreen:
+        	xor ax, ax                        ; Clear the AX register
+        	stosw                             ; Store the contents of the AX register at ES:DI, and increment the value of DI
+        	loop .clearscreen                 ; Loop as long as CX is non-zero (CX is decremented everytime loop is executed)
+
+        mov di, 0                                 ; Having cleared the screen, lets move to the beginning of the screen and display the welcome message
+	mov si, Welcome_Message                   ; Load the string pointer into the SI register
+	.printchar:
+        	lodsb                             ; Load the byte from the address in SI into AL, then increment the address in SI
+        	cmp al, 0                         ; Have we reached the end of the string ?
+        	je .done                          ; If so, break out of the loop
+        	mov ah, 0x0A                      ; We want to print characters in bright green - this is the color code
+        	stosw                             ; Store the AX register (color code + character) into the video buffer
+        	jmp .printchar                    ; Continue the printing loop
+	.done:                                    ; Printing is done
+
+        pop es                                    ; Restore the ES register back to its original segment address
+        popa                                      ; Restore the original state of the general-purpose registers
+        ret                                       ; Return control to the kernel
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
