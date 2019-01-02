9356d82dcbf7a091cee53896806848e392bdbcf9
Adish Vartak <avartak@cern.ch>
2019-01-02 18:33:06 +0530

    First functional, separate kernel

--- bootloader.asm
+++ bootloader.asm
@@ -1,4 +1,4 @@
-; We are creating a very simple operating system - AVOS v0.2.0 
+; We are creating a very simple operating system - AVOS
 ; It's aim in life is to print a welcome message and then halt the system
 ; We start with the bootloader - and a very basic one
 ; Let us recall how we get here :
@@ -58,14 +58,12 @@ Boot:
 	; We will use the BIOS interrupt routine 0x13 to load this sector at memory location 0x7E00 (just following this bootloader)
 	; We will first read the drive parameters using the 'GetDriveInfo' function
 	; We will then read the kernel from disk using the 'LoadKernel' function 
-	; This kernel is dumb -- it contains no executable code, just a display string that the Print function will read and display
+	; This kernel is our first executable kernel. It prints a wekcome message
 
 	call GetDriveInfo
 	mov si, Kernel_Start                      ; LoadKernel function will copy the kernel image from disk to ES:SI
 	call LoadKernel
-	call Print                                ; Call Print function that displays the kernel message
-	cli                                       ; Clear all interrupts so that we won't be disturbed            
-	hlt                                       ; Halt the system	
+	jmp Kernel_Start
 
 ; The GetDriveInfo function - obtain the drive parameters
 
@@ -125,48 +123,6 @@ LoadKernel:
 	
 	popa                                      ; Restore the original state of the general-purpose registers
 	ret                                       ; Return control to the bootloader
-
-; The Print function - displays a string on screen
-; We will write directly to the video buffer
-; The function first clears the scree, then displays the welcome message
-
-Print:
-        pusha                                     ; We start by pushing all the general-purpose registers onto the stack
-                                                  ; This way we can restore their values after the function returns 
-        push es                                   ; Push the ES register onto the stack - we will use it to access the video memory
-        mov ax, 0xB800                            ; In the PC architecture the video buffer sits at 0xB8000
-        mov es, ax                                ; We put the ES segment at that location
-        mov di, 0                                 ; We start writing from the beginning of the video buffer
-        mov ax, 80                                ; There are 80 columns
-        mov bx, 25                                ; And 25 rows
-        mul bx                                    ; Total number of characters we can print: 80x25
-        mov cx, ax                                ; Set this number in the counter register - to be used for clearing the creen
-
-        .clearscreen:
-			xor ax, ax                            ; Clear the AX register
-			stosw                                 ; Store the contents of the AX register at ES:DI, and increment the value of DI
-			loop .clearscreen                     ; Loop as long as CX is non-zero (CX is decremented everytime loop is executed)
-
-		mov di, 0                                 ; Having cleared the screen, lets move to the beginning of the screen and display the welcome message
-		push ax
-		mov ax, 0
-		mov ds, ax
-		mov si, Kernel_Start                      ; Load the string pointer into the SI register
-		pop ax
-
-		.printchar:
-			lodsb                                 ; Load the byte from the address in SI into AL, then increment the address in SI
-			cmp al, 0                             ; Have we reached the end of the string ?
-			je .done                              ; If so, break out of the loop
-			mov ah, 0x0A                          ; We want to print characters in bright green - this is the color code
-			stosw                                 ; Store the AX register (color code + character) into the video buffer
-			jmp .printchar                        ; Continue the printing loop
-		.done:                                    ; Printing is done
-		
-		pop es                                    ; Restore the ES register back to its original segment address
-		popa                                      ; Restore the original state of the general-purpose registers
-		ret                                       ; Return control to the kernel
-
 
 
 Sectors_Per_Track dw 18
--- kernel.asm
+++ kernel.asm
@@ -12,6 +12,62 @@ BITS 16
 times 512 db 0
 
 ; Starting point of the kernel
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Kernel:
+
+	; We now call the 'Print' function to display our welcome message (function defined later)
+	; After printing the welcome message the OS has performed its goal in life
+
+	call Print
+	cli                                       ; Clear all interrupts so that we won't be disturbed            
+	hlt                                       ; Halt the system	
+
+; The Print function - displays a string on screen
+; We will write directly to the video buffer
+; The function first clears the screen, then displays the welcome message
+
+Print:
+	pusha                                     ; We start by pushing all the general-purpose registers onto the stack
+	                                          ; This way we can restore their values after the function returns 
+	push es                                   ; Push the ES register onto the stack - we will use it to access the video memory
+	mov ax, 0xB800                            ; In the PC architecture the video buffer sits at 0xB8000
+	mov es, ax                                ; We put the ES segment at that location
+	mov di, 0                                 ; We start writing from the beginning of the video buffer
+	mov ax, 80                                ; There are 80 columns
+	mov bx, 25                                ; And 25 rows
+	mul bx                                    ; Total number of characters we can print: 80x25
+	mov cx, ax                                ; Set this number in the counter register - to be used for clearing the creen
+	
+	.clearscreen:
+	    xor ax, ax                            ; Clear the AX register
+	    stosw                                 ; Store the contents of the AX register at ES:DI, and increment the value of DI
+	    loop .clearscreen                     ; Loop as long as CX is non-zero (CX is decremented everytime loop is executed)
+	
+	mov di, 0                                 ; Having cleared the screen, lets move to the beginning of the screen and display the welcome message
+	push ax
+	mov ax, 0
+	mov ds, ax
+	mov si, Welcome_Message                   ; Load the string pointer into the SI register
+	pop ax
+	
+	.printchar:
+	    lodsb                                 ; Load the byte from the address in SI into AL, then increment the address in SI
+	    cmp al, 0                             ; Have we reached the end of the string ?
+	    je .done                              ; If so, break out of the loop
+	    mov ah, 0x0A                          ; We want to print characters in bright green - this is the color code
+	    stosw                                 ; Store the AX register (color code + character) into the video buffer
+	    jmp .printchar                        ; Continue the printing loop
+	.done:                                    ; Printing is done
+	
+	pop es                                    ; Restore the ES register back to its original segment address
+	popa                                      ; Restore the original state of the general-purpose registers
+	ret                                       ; Return control to the kernel
+
+
+
+
+; Starting point of the kernel
 Welcome_Message db 'Welcome to AVOS!', 0
 
 ; Adding a zero padding to the boot sector
