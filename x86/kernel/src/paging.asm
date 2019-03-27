3b07622e1309d5e7240a50063756d3fd372f026f
Adish Vartak <avartak@cern.ch>
2019-03-27 10:51:48 +0100

    Formatting

--- x86/kernel/src/paging.asm
+++ x86/kernel/src/paging.asm
@@ -6,33 +6,26 @@ extern Paging_directory
 
 global Paging_EnablePGBitInCR0
 Paging_EnablePGBitInCR0:
-
 	mov  eax, cr0
 	or   eax, 0x80000000
 	mov  cr0, eax
-
 	ret
 
 global Paging_LoadDirectory
 Paging_LoadDirectory:
-
 	mov  eax, [esp+4]
 	mov  cr3, eax
-
 	ret
 
 global Paging_SwitchToHigherHalf
 Paging_SwitchToHigherHalf:
-
     mov  eax, High_Memory
     jmp  eax
     High_Memory:
     mov  [Paging_directory], DWORD 0
-
     mov  eax, [esp]
     add  eax, HIGHER_HALF_OFFSET
     mov  [esp], eax
     add  esp, HIGHER_HALF_OFFSET
-
     ret
 
