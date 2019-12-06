3722a9bb039caa963b7c9f9b81a20ca8b654b26e
Adish Vartak <avartak@cern.ch>
2019-12-06 22:47:16 +0100

    Some formatting

--- boot/x86/BIOS/include/mbi.h
+++ boot/x86/BIOS/include/mbi.h
@@ -5,16 +5,16 @@
 #include <boot/general/include/multiboot.h>
 #include <boot/general/include/boot.h>
 
-extern bool      Multiboot_SaveBootLoaderInfo    (uintptr_t mbi_addr);
-extern bool      Multiboot_SaveBootCommand       (uintptr_t mbi_addr);
-extern bool      Multiboot_SaveMemoryMaps        (uintptr_t mbi_addr);
-extern bool      Multiboot_SaveBasicMemoryInfo   (uintptr_t mbi_addr);
-extern bool      Multiboot_SaveBootDeviceInfo    (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
-extern bool      Multiboot_SaveGraphicsInfo      (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
-extern bool      Multiboot_SaveAPMInfo           (uintptr_t mbi_addr);
-extern bool      Multiboot_SaveSMBIOSInfo        (uintptr_t mbi_addr);
-extern bool      Multiboot_SaveACPIInfo          (uintptr_t mbi_addr, bool old);
-extern bool      Multiboot_SaveLoadBaseAddress   (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
-extern bool      Multiboot_SaveInfo              (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
+extern bool  Multiboot_SaveBootLoaderInfo       (uintptr_t mbi_addr);
+extern bool  Multiboot_SaveBootCommand          (uintptr_t mbi_addr);
+extern bool  Multiboot_SaveMemoryMaps           (uintptr_t mbi_addr);
+extern bool  Multiboot_SaveBasicMemoryInfo      (uintptr_t mbi_addr);
+extern bool  Multiboot_SaveAPMInfo              (uintptr_t mbi_addr);
+extern bool  Multiboot_SaveSMBIOSInfo           (uintptr_t mbi_addr);
+extern bool  Multiboot_SaveACPIInfo             (uintptr_t mbi_addr, bool old);
+extern bool  Multiboot_SaveBootDeviceInfo       (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
+extern bool  Multiboot_SaveGraphicsInfo         (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
+extern bool  Multiboot_SaveLoadBaseAddress      (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
+extern bool  Multiboot_SaveInfo                 (uintptr_t mbi_addr, struct Boot_Kernel_Info* kernel_info);
 
 #endif
