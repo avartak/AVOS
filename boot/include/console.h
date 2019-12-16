37bba1a06d7c3ca18e49c03cbf5f7af567ee509a
Adish Vartak <avartak@cern.ch>
2019-12-16 11:52:40 +0530

    Adding description to console.h

--- boot/include/console.h
+++ boot/include/console.h
@@ -1,3 +1,26 @@
+/*
+
+These functions are used for text I/O if needed in the bootloader
+
+Several functions that are used to display characters on screen write directly to the linear text buffer located at 0xB8000
+- It is assumed that the video is set to 80x25 text mode by BIOS
+- This is not explicitly enforced as of now, but could be done (e.g. by setting the VGA mode to 3 or something equivalent in the Console_PrintBanner function) 
+
+* Console_ClearScreen         : Clears the contents of the screen to show a complete blank
+* Console_MakeCursorVisible   : Show the default cursor (underline, blinking) using INT 0x10, AH=0x01 ; achieved by setting CX=0x0607 when calling the BIOS routine
+* Console_MakeCursorInvisible : Hide the using INT 0x10, AH=0x01 ; achieved by setting CX=0x2607 when calling the BIOS routine
+* Console_SetCursorPosition   : Set the cursor position to a given line & column using INT 0x10, AH=0x02 ; set DH=row, DL=column, BH=0 (page number)
+* Console_PrintNum            : Print 32-bit unsigned integer to a given line & column on screen with given color attribute; number starts with 0x and always shows 8 numerals (0-padded)
+* Console_PrintChar           : Print a character to a given line & column on screen with given color attribute
+* Console_PrintString         : Print a 0-terminated string to screen starting at a given line & column with given color attribute
+* Console_ReadChar            : Read a character from keyboard using INT 0x16, AH=0x00 ; the read character is stored in AL
+
+* Console_PrintBanner         : First clears the screen, then prints the AVBL banner on the first line, and makes the cursor invisible
+* Console_PrintError          : Prints an error string on line 23 in red color and returns the boolean passed as the second argument (useful when returning a boolean status + message)
+* Console_ReadCommand         : Reads a command string from keyboard (up to 0x400 characters) and displays it at the AVBL command prompt
+
+*/
+
 #ifndef BOOT_CONSOLE_H
 #define BOOT_CONSOLE_H
 
