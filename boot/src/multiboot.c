04c1a28f004fddb376177fbd49ffaae954f22773
Adish Vartak <avartak@cern.ch>
2019-12-21 21:16:58 +0530

    Minor update

--- boot/src/multiboot.c
+++ boot/src/multiboot.c
@@ -718,15 +718,15 @@ bool Multiboot_SaveInfo(uint32_t mbi_addr, struct Boot_KernelInfo* kernel_info) 
 
 bool Multiboot_Boot(uint32_t mbi_addr, struct Boot_KernelInfo* kernel_info) {
 
-    Console_PrintBanner();
-
-    if (!Multiboot_CreateEmptyMBI(mbi_addr             )) return Console_PrintError("Error creating multiboot information record", false);
-    if (!Multiboot_SaveMemoryMaps(mbi_addr             )) return Console_PrintError("Error saving memory maps",                    false);
-    if (!Multiboot_LoadKernelFile(mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS kernel file",               false);
-    if (!Multiboot_LoadKernel    (mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS kernel",                    false);
-    if (!Multiboot_LoadModules   (mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS kernel modules",            false);
-    if (!Multiboot_SaveInfo      (mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS boot information",          false);
-
-    return true;
+	Console_PrintBanner();
+	
+	if (!Multiboot_CreateEmptyMBI(mbi_addr             )) return Console_PrintError("Unable to create multiboot information record", false);
+	if (!Multiboot_SaveMemoryMaps(mbi_addr             )) return Console_PrintError("Unable to load memory maps",                    false);
+	if (!Multiboot_LoadKernelFile(mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS kernel file",                 false);
+	if (!Multiboot_LoadKernel    (mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS kernel",                      false);
+	if (!Multiboot_LoadModules   (mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS kernel modules",              false);
+	if (!Multiboot_SaveInfo      (mbi_addr, kernel_info)) return Console_PrintError("Unable to load OS boot information",            false);
+	
+	return true;
 }
 
